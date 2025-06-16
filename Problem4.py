8476602aba8e93840e9babf04ce32ef6a8fde14f
Yehuda-levy923 <yehuda.levy08@gmail.com>
2025-06-17 02:02:50 +0300

    made 4 also return the 2 denominators

--- Problem4.py
+++ Problem4.py
@@ -3,4 +3,7 @@ def is_palindrome(n):
 
 if __name__ == '__main__':
     n = int(input("Enter a number: "))
-    print(max(i * j for i in range(10**(n-1), 10**n) for j in range(i, 10**n) if is_palindrome(i * j)))
+    max_pal, f = (
+        max(((i * j, (i, j)) for i in range(10 ** (n - 1), 10 ** n) for j in range(i, 10 ** n) if is_palindrome(i * j)),
+        key=lambda x: x[0]))
+    print(f[0], f[1], max_pal)
