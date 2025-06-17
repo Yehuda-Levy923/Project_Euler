80999c7e64fe3250ab0afb7782f639ab4515d81a
Yehuda-levy923 <yehuda.levy08@gmail.com>
2025-06-17 10:55:48 +0300

    made a successful Problem7.py

---
+++ Problem7.py
@@ -0,0 +1,25 @@
+def is_prime(num):
+    if num == 2:
+        return True
+    if num < 2 or num % 2 == 0:
+        return False
+    for i in range(3, int(num**0.5)+1, 2):
+        if num % i == 0:
+            return False
+    return True
+
+def nth_prime(num):
+    if n == 1:
+        return 2
+    how_many = 1
+    prime = 3
+    while True:
+        if is_prime(prime):
+            how_many += 1
+            if how_many == num:
+                return prime
+        prime += 2
+
+if __name__ == '__main__':
+    n = int(input("Enter a number: "))
+    print(nth_prime(n))
